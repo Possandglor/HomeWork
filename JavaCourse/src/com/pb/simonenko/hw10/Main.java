af0b5e264d26935c05357fad1435e9dacab5d3df
Симоненко Владислав Сергеевич <vladislav.simonenko@privatbank.ua>
2021-11-28 20:48:15 +0200

    HW10

--- JavaCourse/src/com/pb/simonenko/hw10/Main.java
+++ JavaCourse/src/com/pb/simonenko/hw10/Main.java
@@ -1,6 +1,9 @@
 package com.pb.simonenko.hw10;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
     NumBox<Integer> intBox = new NumBox<Integer>(10);
     NumBox<Float> floatBox = new NumBox<Float>(10);
 }
